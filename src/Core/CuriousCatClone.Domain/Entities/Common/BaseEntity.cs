9dc179209301e7a3b234459ece895fee8ff6e3d4
Anar Bayramov <anar.bayram@yahoo.com>
2024-06-25 17:11:05 +0400

    feat: Add project references and BaseEntity class

---
+++ src/Core/CuriousCatClone.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CuriousCatClone.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
